5c660450aca2a570eb59cc2984a7ef55f794b7fb
tai0112 <112180056+tai0112@users.noreply.github.com>
2025-01-18 23:47:22 +0700

    Update detail page

--- ProjectQuanLyBanHang/Controllers/SanPhamController.cs
+++ ProjectQuanLyBanHang/Controllers/SanPhamController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AngleSharp.Io;
 using Microsoft.Ajax.Utilities;
 using ProjectQuanLyBanHang.Models;
 
@@ -15,6 +17,20 @@ namespace ProjectQuanLyBanHang.Controllers
         public ActionResult ChiTietSanPham(int id)
         {
             var sanPham = db.sanPhamChiTiets.Where(o => o.SanPhamChiTietId == id).FirstOrDefault();
+            var sanPhamLienQuan = db.sanPhamChiTiets.Where(o => o.SanPham.LoaiSanPham.MaLoaiSanPham == sanPham.SanPham.LoaiSanPham.MaLoaiSanPham).Count();
+            List<SanPhamChiTiet> lstSanPhamChiTiet = new List<SanPhamChiTiet>();
+            for (int i = 0; i < 3; i++)
+            {
+                var random = new Random().Next(0, sanPhamLienQuan);
+                SanPhamChiTiet randomProduct = db.sanPhamChiTiets.Where(o => o.SanPham.TrangThai == true).OrderBy(o => o.SanPham.LoaiSanPham.MaLoaiSanPham).Skip(random).FirstOrDefault();
+                lstSanPhamChiTiet.Add(randomProduct);
+            }
+            ViewBag.ProductSimilar = lstSanPhamChiTiet;
+
+            foreach(var item in lstSanPhamChiTiet)
+            {
+                Debug.WriteLine(item.SanPham.AnhSanPhams.FirstOrDefault().UrlAnh);
+            }
             return View(sanPham);
         }
     }
